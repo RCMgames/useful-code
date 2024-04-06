b99f0ed89ca72a2b44377a694841b02170297308
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2024-04-06 17:37:01 +0000

    add readme

---
+++ ICM20948/ICM20948_helper.h
@@ -0,0 +1,17 @@
+/**
+ * This file is meant to make it even easier to use Sparkfun's ICM20948 library for Arduino
+*/
+#ifndef ICM20948_helper_H
+#define ICM20948_helper_H
+
+
+
+boolean setupICM20948(){
+
+
+}
+boolean nibbleStartImu(){
+    return setupICM20948();
+}
+
+#endif
---
+++ ICM20948/README.md
@@ -0,0 +1,6 @@
+
+## Installation
+Add this line to `lib_deps` inside your platformio.ini file.
+```sparkfun/SparkFun 9DoF IMU Breakout - ICM 20948 - Arduino Library@^1.2.12```
+
+copy the `ICM20948_helper.h` file into the `RCMv2` folder that `RCMv2.ino` is in.
