07a7052eab9c0380e01d5b3f308d71d987bb8e58
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2024-04-24 21:16:31 -0400

    Create RCM-Nibble.h

---
+++ boards/RCM-Nibble.h
@@ -0,0 +1,9 @@
+JMotorDriverEsp32Servo servo1Driver = JMotorDriverEsp32Servo(port1);
+JMotorDriverEsp32Servo servo2Driver = JMotorDriverEsp32Servo(port2);
+JMotorDriverEsp32Servo servo3Driver = JMotorDriverEsp32Servo(port3);
+JMotorDriverEsp32Servo servo4Driver = JMotorDriverEsp32Servo(port4);
+
+JMotorDriverTMC7300 motor1Driver = JMotorDriverTMC7300(portA);
+JMotorDriverTMC7300 motor2Driver = JMotorDriverTMC7300(portB);
+JMotorDriverTMC7300 motor3Driver = JMotorDriverTMC7300(portC);
+JMotorDriverTMC7300 motor4Driver = JMotorDriverTMC7300(portD);
